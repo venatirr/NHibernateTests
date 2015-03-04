be9a5383ce924a6012c1243757839d2ca994853b
florin <florincapra@gmail.com>
2015-03-05 00:14:29 +0200

    initial changeset for this solution

---
+++ NHibernateTests/Mappings/Actor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using FluentNHibernate.Mapping;
+
+namespace NHibernateTests.Mappings
+{
+	public class Actor
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual IList Movies { get; set; }
+	}
+
+	public class ActorMap : ClassMap<Actor>
+	{
+		public ActorMap()
+		{
+			Id(x => x.Id);
+			Map(x => x.Name);
+
+			HasManyToMany<Movie>(x => x.Movies)
+				.Table("Cast")
+				.ParentKeyColumn("ActorId")
+				.ChildKeyColumn("MovieId")
+				.LazyLoad()
+				.Cascade.All();
+		}
+	}
+}
---
+++ NHibernateTests/Mappings/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using FluentNHibernate.Mapping;
+
+namespace NHibernateTests.Mappings
+{
+	public class Movie
+	{
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual IList Actors { get; set; }
+	}
+
+	public class MovieMap : ClassMap<Movie>
+	{
+		public MovieMap()
+		{
+			Id(x => x.Id);
+			Map(x => x.Name);
+
+			HasManyToMany<Actor>(x => x.Actors)
+				.Table("Cast")
+				.ParentKeyColumn("ActorId")
+				.ChildKeyColumn("MovieId")
+				.Inverse();
+		}
+	}
+}
